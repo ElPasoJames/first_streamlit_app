3cf45911a08c561a768975a23580c50ae20c9066
Wonderbred <123215943+ElPasoJames@users.noreply.github.com>
2023-10-09 16:25:24 -0600

    Update streamlit_app.py
    change table index from number to fruit name

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,8 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 #import data from s3 DB to create a table
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# change the table index to use fruit names rather than table number
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #enables customer to pick a friot they want to add from dropdown
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
