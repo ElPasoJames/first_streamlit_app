ee2a7e8b73293ce73a4ff5ca3d8fffcfa3f7cabd
Wonderbred <123215943+ElPasoJames@users.noreply.github.com>
2023-10-09 16:36:21 -0600

    Update streamlit_app.py
    added fruits selected and fruits to show

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,13 +12,19 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 #import data from s3 DB to create a table
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
 # change the table index to use fruit names rather than table number
 my_fruit_list = my_fruit_list.set_index('Fruit')
+
 #gives a few starter suggestions in the drop down selector
-streamlit.multiselect("Pick some fruits:", list (my_fruit_list.index), ['Avocado','Strawberries'])
+#streamlit.multiselect("Pick some fruits:", list (my_fruit_list.index), ['Avocado','Strawberries'])
+#added fruits selected the the code
+fruits_selected = streamlit.multiselect("Pick some fruits:", list (my_fruit_list.index), ['Avocado','Strawberries'])
 
 #enables customer to pick a friot they want to add from dropdown
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+#streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 #creates the actual table from fruit data
-streamlit.dataframe(my_fruit_list)
+#streamlit.dataframe(my_fruit_list)
+#chaged my_fruit_list to fruits_to_show
+streamlit.dataframe(fruits_to_show)
