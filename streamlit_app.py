3063104e36a1c70e40b8397def96fbdd3134d388
Wonderbred <123215943+ElPasoJames@users.noreply.github.com>
2023-10-10 03:30:01 -0600

    Update streamlit_app.py
    add snowflake query

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,3 +46,11 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 # enable pandas to fruityvice
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
+
+#query data for snowflake
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
