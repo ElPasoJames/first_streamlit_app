a986530a466e60972fcd1acc2ffeefe89e04af7d
Wonderbred <123215943+ElPasoJames@users.noreply.github.com>
2023-10-10 15:24:58 -0600

    Update streamlit_app.py
    added : if not fruit choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 #create text block for user to enter fruit choice
 try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?')
-    if not fruit_choice
+    if not fruit_choice:
         streamlit.error("Please select a fruit to get information")
     else:
         # Diisplay fruityvice API responce
