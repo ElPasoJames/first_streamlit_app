4a48e0eabbe7b3526f47fed857c7de395ff58be8
Wonderbred <123215943+ElPasoJames@users.noreply.github.com>
2023-10-10 01:49:40 -0600

    Update streamlit_app.py
    move API response line

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,9 +24,6 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list (my_fruit_list.index), ['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-# Diisplay fruityvice API responce
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
 #enables customer to pick a friot they want to add from dropdown
 #streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
@@ -34,3 +31,7 @@ streamlit.text(fruityvice_response)
 #streamlit.dataframe(my_fruit_list)
 #chaged my_fruit_list to fruits_to_show
 streamlit.dataframe(fruits_to_show)
+
+# Diisplay fruityvice API responce
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
