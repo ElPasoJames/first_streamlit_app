588475b3dfd6c416a9420be93655a879635bfb65
Wonderbred <123215943+ElPasoJames@users.noreply.github.com>
2023-10-10 15:24:06 -0600

    Update streamlit_app.py
    added try, else, except statements

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,16 +38,20 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 #create text block for user to enter fruit choice
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice
+        streamlit.error("Please select a fruit to get information")
+    else:
+        # Diisplay fruityvice API responce
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+        ######streamlit.text(fruityvice_response.json())
+        # enable pandas to fruityvice
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlit.error()
                 
-# Diisplay fruityvice API responce
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-######streamlit.text(fruityvice_response.json())
-# enable pandas to fruityvice
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
-
 #query data for snowflake
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
