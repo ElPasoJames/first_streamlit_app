7f9b8d024b29d007df5e3da84a1b314c3d9304dd
Wonderbred <123215943+ElPasoJames@users.noreply.github.com>
2023-10-10 01:57:13 -0600

    Update streamlit_app.py
    remove/comment out stremlit.text(fruit....json) line
    Modify response URL

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,8 +35,8 @@ streamlit.dataframe(fruits_to_show)
 #display fruityvice response
 streamlit.header("Fruityvice Fruit Advice!")
 # Diisplay fruityvice API responce
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+######streamlit.text(fruityvice_response.json())
 # enable pandas to fruityvice
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
