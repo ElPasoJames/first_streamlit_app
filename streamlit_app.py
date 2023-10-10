f74a9cdfa53de8c9e5741d311f6426e1eb5737a2
Wonderbred <123215943+ElPasoJames@users.noreply.github.com>
2023-10-10 01:53:57 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,3 +37,6 @@ streamlit.header("Fruityvice Fruit Advice!")
 # Diisplay fruityvice API responce
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+# enable pandas to fruityvice
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
