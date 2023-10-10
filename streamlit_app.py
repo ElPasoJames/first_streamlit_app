b56351b8391d5df20227a506a242605035ec89d3
Wonderbred <123215943+ElPasoJames@users.noreply.github.com>
2023-10-10 02:02:23 -0600

    Update streamlit_app.py
    missed a closing )

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 #create text block for user to enter fruit choice
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice
+streamlit.write('The user entered ', fruit_choice)
                 
 # Diisplay fruityvice API responce
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
