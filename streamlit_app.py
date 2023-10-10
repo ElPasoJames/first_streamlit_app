01f95447b8402f293730e9da5280866413689121
Wonderbred <123215943+ElPasoJames@users.noreply.github.com>
2023-10-10 16:22:50 -0600

    Update streamlit_app.py
    modify code to functions

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,12 +55,24 @@ def get_fruityvice_data(this_fruit_choice):
     #streamlit.error()
                 
 #query data for snowflake
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from FRUIT_LOAD_LIST")
-my_data_rows = my_cur.fetchall()
-streamlit.header("Hello from Snowflake:")
-streamlit.dataframe(my_data_rows)
+#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cur = my_cnx.cursor()
+#my_cur.execute("select * from FRUIT_LOAD_LIST")
+#my_data_rows = my_cur.fetchall()
+#streamlit.header("The fruit load list contains:")
+#streamlit.dataframe(my_data_rows)
+
+streamlit.header("The fruit load list contains:")
+#snowflake related functions
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("select * from fruit_load_list")
+        return my_cur.fetchall()
+#add nutton to load fruit
+if streamlit.button('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
 
 #add_my_fruit = streamlit.multiselect("What fruits would you like to add?:", list (my_fruit_list.index))
 #fruits_to_show = my_fruit_list.loc[fruits_selected]
